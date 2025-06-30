d8f2adfe7b248132297c4b5073e55b67e70d7aaf
Anjali <sj5065070@gmail.com>
2025-07-01 00:16:30 +0530

    Create script.js

---
+++ task-1/script.js
@@ -0,0 +1,16 @@
+// Task 1: Check if a number is positive, negative, or zero
+
+
+let num=prompt("enter a any number :")
+
+
+if(num<0){
+    console.log("number is negative");
+}
+
+else if (num>0) {
+    console.log("number is positive")
+} 
+else {
+    console.log("number is 0")
+}
