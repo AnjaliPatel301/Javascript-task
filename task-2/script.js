f339e605b0c6f6bc2befd9e7c1e19c66015864d5
Anjali <sj5065070@gmail.com>
2025-07-01 00:17:43 +0530

    Update script.js

--- task-2/script.js
+++ task-2/script.js
@@ -1,1 +1,12 @@
 
+// Task 2: Check if a number is even or odd/
+
+let num=prompt("enter a number :")
+
+if(num%2==0){
+    console.log("number is even")
+}
+
+else if(num%2!==0){
+    console.log("number is odd")
+}
