7dd36282e739db631c9e48f59512445308855dc8
Anjali <sj5065070@gmail.com>
2025-07-01 00:20:50 +0530

    Create script.js

---
+++ task-5/script.js
@@ -0,0 +1,19 @@
+// Task 5: Grade calculator
+
+let num=prompt("enter a num")
+
+ if (num >= 60) {
+    console.log("grade a")
+  } 
+  
+  else if (num >= 50) {
+    console.log("garde b")
+ } 
+      
+ else if (num >= 40) {
+           console.log("grade c")
+ }
+
+ else {
+        console.log("faild")
+        }
