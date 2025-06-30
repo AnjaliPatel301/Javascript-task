3fc84a6b64ee2bac5f298309b5e8243ae898a776
Anjali <sj5065070@gmail.com>
2025-07-01 00:19:44 +0530

    Create script.js

---
+++ task-4/script.js
@@ -0,0 +1,11 @@
+// Task 3: Find the largest of two numbers
+
+let num1=prompt("enter num1 :")
+let num2=prompt("enter num2 :")
+
+if(num1>num2){
+    console.log("num1 is largest")
+}
+else{
+    console.log("num2 is largest")
+}
