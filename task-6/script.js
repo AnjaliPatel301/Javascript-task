533e82364ac91ab30a47769f24301070ce4ccc4f
Anjali <sj5065070@gmail.com>
2025-07-02 00:02:10 +0530

    Update script.js

--- task-6/script.js
+++ task-6/script.js
@@ -1,1 +1,13 @@
+et num1=prompt.("enter num1...:-")
+let num2=prompt.("enter num2...:-")
+
+if(num1<num2){
+    console.log(prompt(("too high!😒")))
+}
+else if(num1>num2){
+    console.log(prompt("too low!🫠"))
+}
+else if(num1==num2){
+    console.log("Congratulations!🎊")
+}
 
