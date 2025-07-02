2757a1fcb8aa2ed4e26ddd4afa4c2ef02459b19f
Anjali <sj5065070@gmail.com>
2025-07-02 10:18:23 +0530

    Update script.js

--- task-6/script.js
+++ task-6/script.js
@@ -1,5 +1,5 @@
-et num1=prompt.("enter num1...:-")
-let num2=prompt.("enter num2...:-")
+let num1=prompt("enter num1...:-")
+let num2=prompt("enter num2...:-")
 
 if(num1<num2){
     console.log(prompt(("too high!😒")))
